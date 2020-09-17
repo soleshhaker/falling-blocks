4412b2b0bb9251dcc4a31e357d65e8ecec08f47f
soleshhaker <tobi1560@t.pl>
2020-09-17 20:06:12 +0200

    initial commit

---
+++ falling blocks/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 7f;
+
+    public float screenWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float playerHalfWidth = transform.localScale.x / 2f;
+        screenWidth = Camera.main.aspect * Camera.main.orthographicSize + playerHalfWidth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float X = Input.GetAxisRaw("Horizontal");
+        float velocity = X * speed;
+        transform.Translate(Vector3.right * velocity * Time.deltaTime);
+
+        if (transform.position.x < -screenWidth)
+        {
+            transform.position = new Vector3(screenWidth, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > screenWidth)
+        {
+            transform.position = new Vector3(-screenWidth, transform.position.y, transform.position.z);
+        }
+
+      
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ falling blocks/Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Block;
+    Vector2 screenWidth;
+    public float secondsToSpawn = 1;
+    public float nextSpawn;
+
+    public Vector2 spawnMinMax;
+    public float spawnAngleMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenWidth = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn) {
+            nextSpawn = Time.time + secondsToSpawn;
+
+            float spawnAngle = Random.Range(-spawnAngleMax, spawnAngleMax);
+            float spawnSize = Random.Range(spawnMinMax.x, spawnMinMax.y);
+        Vector2 spawnPosition = new Vector2(Random.Range(-screenWidth.x, screenWidth.x), screenWidth.y + spawnSize);
+       GameObject newBlock = (GameObject)Instantiate(Block, spawnPosition, Quaternion.Euler(Vector3.forward * spawnAngle));
+            newBlock.transform.localScale = Vector3.one * spawnSize;
+
+
+        }
+
+    }
+   
+}
